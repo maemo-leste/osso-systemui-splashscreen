4e21ab92fcaa683527030c5898ca500f5499ca8b
Pali Rohár <pali.rohar@gmail.com>
2013-09-19 13:51:34 +0200

    Add splashscreen-dbus-names.h

---
+++ splashscreen-dbus-names.h
@@ -0,0 +1,9 @@
+
+
+#define SYSTEMUI_SPLASHSCREEN_OPEN_REQ "splashscreen_open"
+#define SYSTEMUI_SPLASHSCREEN_CLOSE_REQ "splashscreen_close"
+
+typedef enum {
+	SPLASHSCREEN_ENABLE_BOOTUP = 0,
+	SPLASHSCREEN_ENABLE_SHUTDOWN = 1
+} splashscreen_t;
